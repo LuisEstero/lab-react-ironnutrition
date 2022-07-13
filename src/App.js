637ccc1574e061cc866e590e5184a6a298a51ad8
Luis Estero <luisestero@gmail.com>
2022-07-13 20:23:56 +0200

    Iteración 1 - 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 // src/App.js
 import './App.css';
+import foods from './foods.json';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import React, { useState } from 'react';
+
+
+function Example() {
+  
+  const Example = (props) => { 
+  }
+  return (
+    <div><div>
+    <p> FOOD_NAME_HERE </p>
+    <img src="FOOD_IMAGE_HERE" width={0} />
+  </div>
+      <Row>
+        <Col>
+          <Divider>Fancy Input</Divider>
+          <Input value={''} onChange={() => {}} />
+        </Col>
+
+        <Col>
+          <Card title={'Fancy Card'}>
+            <Button onClick={() => {}}>Fancy Button</Button>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+
 
 function App() {
   return <div className="App"></div>;
---
+++ src/componentes/FoodBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+<FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
+
+
+export default FoodBox;
